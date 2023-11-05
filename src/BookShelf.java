b6c8258f392e79dfde2948efcca30f0abfe755eb
oleghenkel <oleg.henkel75@gmail.com>
2023-11-05 13:36:04 +0100

    Bilet 12

--- src/BookShelf.java
+++ src/BookShelf.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 public class BookShelf {
     public static List<List<String>> distributeBooks(List<String> books) {
+
         Collections.sort(books);
+
         int numberOfShelves = 5;
         List<List<String>> shelves = new ArrayList<>();
         for (int i = 0; i < numberOfShelves; i++) {
             shelves.add(new ArrayList<>());
 
         }
+
         int currentShelf = 0;
         for(String book : books){
             shelves.get(currentShelf).add(book);
